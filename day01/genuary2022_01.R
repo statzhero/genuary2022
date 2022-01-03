bb22b3ffc6a15739a0bb105153daf5e0f5d975f2
statzhero <panoramadata@gmail.com>
2022-01-03 13:36:36 -0500

    Initial commit

---
+++ day01/genuary2022_01.R
@@ -0,0 +1,13 @@
+#' Genuary 2022
+#' Author: Ulrich Atz
+#' Date: 2022-01-03
+#' https://genuary.art/prompts
+#' --------------------------------------------
+#' Draw 10,000 of something.
+#' --------------------------------------------
+
+# Load packages and helper scripts
+library(tidyverse)
+library(camcorder)
+gg_record(dir = 'day01/tmp', dpi = 320, width = 6, height = 6)
+
